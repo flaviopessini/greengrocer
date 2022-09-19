effcb8ac236a579fca0c8e301c12c04ce2a87127
Flávio Henrique Pessini <flavio.pessini@outlook.com>
2022-09-18 21:45:00 -0300

    requisições auth pelo controller e uso do JsonSerializable

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,19 +1,63 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class UserModel {
-  String name;
+  String? id;
 
-  String phone;
+  String? name;
 
-  String email;
+  String? phone;
 
-  String password;
+  String? email;
 
-  String cpf;
+  String? password;
+
+  String? cpf;
+
+  String? token;
 
   UserModel({
-    required this.name,
-    required this.phone,
-    required this.email,
-    required this.password,
-    required this.cpf,
+    this.id,
+    this.name,
+    this.phone,
+    this.email,
+    this.password,
+    this.cpf,
+    this.token,
   });
+
+  // factory UserModel.fromMap(Map<String, dynamic> map) {
+  //   return UserModel(
+  //     id: map['id'],
+  //     name: map['fullname'],
+  //     phone: map['phone'],
+  //     email: map['email'],
+  //     password: map['password'],
+  //     cpf: map['cpf'],
+  //     token: map['token'],
+  //   );
+  // }
+  //
+  // Map<String, dynamic> toMap() {
+  //   return {
+  //     'id': id,
+  //     'fullname': name,
+  //     'phone': phone,
+  //     'email': email,
+  //     'password': password,
+  //     'cpf': cpf,
+  //     'token': token,
+  //   };
+  // }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+  @override
+  String toString() {
+    return 'UserModel{name: $name, cpf: $cpf}';
+  }
 }
--- lib/src/pages/auth/controllers/auth_controller.dart
+++ lib/src/pages/auth/controllers/auth_controller.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/auth/repo/auth_repository.dart';
 
 class AuthController extends GetxController {
+  final authRepository = AuthRepository();
+
   RxBool isLoading = false.obs;
 
   Future<void> signIn({required String email, required String password}) async {
     isLoading.value = true;
-    await Future.delayed(const Duration(seconds: 2));
+    await authRepository.signIn(email: email, password: password);
     isLoading.value = false;
   }
 }
