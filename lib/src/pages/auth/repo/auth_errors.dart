5d95d631375e33bc8c82ef24962ecd524bad3aff
Flávio Henrique Pessini <flavio.pessini@outlook.com>
2022-09-22 20:44:29 -0300

    Mensagens de erro de cadastro

--- lib/src/pages/auth/repo/auth_errors.dart
+++ lib/src/pages/auth/repo/auth_errors.dart
@@ -1,9 +1,15 @@
 String authErrorMessage(String? code) {
-  switch(code){
+  switch (code) {
     case 'INVALID_CREDENTIALS':
       return 'E-mail e/ou senha inválidos';
     case 'Invalid session token':
       return 'Token inválido';
+    case 'INVALID_FULLNAME':
+      return 'Ocorreu um erro ao cadastrar usuário: Nome inválido';
+    case 'INVALID_PHONE':
+      return 'Ocorreu um erro ao cadastrar usuário: Telefone inválido';
+    case 'INVALID_CPF':
+      return 'Ocorreu um erro ao cadastrar usuário: CPF inválido';
     default:
       return 'Erro indefinido';
   }
